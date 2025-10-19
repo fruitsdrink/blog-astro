6f6015699e44028fdde678c9c8fe84fea298cf41
fruitsdrink <fruitsdrink@126.com>
2025-10-19 09:35:34 +0800

    Refactor code presentation components and enhance blog post structure
    - Removed the deprecated `CodeView` component and introduced the new `CodeViewer` component for improved code display.
    - Updated the post rendering logic to utilize the new `CodeViewer` component, enhancing the presentation of code snippets in blog posts.
    - Added new blog posts showcasing various loader animations, each with relevant HTML, CSS, and JavaScript files.
    - Improved the overall structure and organization of blog post content for better readability and user experience.

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ import Header from "./Header.astro";
 import Footer from "./Footer.astro";
 import { HtmlPreview } from "./HtmlPreview";
 import { JsFiddlePreview } from "./JsFiddlePreview";
-import CodeView from "./CodeView.astro";
 import Prose from "./Prose.astro";
+import CodeViewer from "./CodeViewer.astro";
 
-export { Head, Header, Footer, HtmlPreview, JsFiddlePreview, CodeView, Prose };
+export { Head, Header, Footer, HtmlPreview, JsFiddlePreview, Prose, CodeViewer };
