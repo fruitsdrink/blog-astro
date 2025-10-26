6badcee8d590d294f3d2e988b46e70275f7bd51c
fruitsdrink <fruitsdrink@126.com>
2025-10-26 09:56:47 +0800

    Add Netlify adapter and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,17 @@ import mdx from '@astrojs/mdx';
 
 import icon from 'astro-icon';
 
+
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), partytown(), mdx(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
