3fef38981392312c5f645cb3caf310da0d321ca1
fruitsdrink <fruitsdrink@126.com>
2025-10-31 23:46:13 +0800

    Enhance post metadata and layout for improved content presentation
    - Updated the post schema to include a tags field for better categorization.
    - Modified the "加载器-双重加载环" post to include tags related to CSS and animation.
    - Improved the post display layout to show tags alongside the publication date for enhanced user experience.
    - Adjusted the content configuration for better structure and readability.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,28 +1,33 @@
-import {  glob } from "astro/loaders";
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  loader: glob({base:"./src/content/posts", pattern: "**/*.{md,mdx}"}),
-  schema:({image})=>{
+  loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) => {
     return z.object({
       title: z.string(),
       description: z.string().optional(),
       date: z.coerce.date(),
-      cover: z.object({
-        image: image().optional(),
-        hiddenInSingle: z.boolean().optional(),
-      }).optional(),
+      tags: z.array(z.string()).optional().default([]),
+      cover: z
+        .object({
+          image: image().optional(),
+          hiddenInSingle: z.boolean().optional(),
+        })
+        .optional(),
       jsFiddle: z.string().optional(),
-      preview: z.object({
-        enabled: z.boolean(),
-        height: z.number().optional(),
-      }).optional(),
+      preview: z
+        .object({
+          enabled: z.boolean(),
+          height: z.number().optional(),
+        })
+        .optional(),
       bilibili: z.string().optional(),
       youtube: z.string().optional(),
-    })
-  }
-})
+    });
+  },
+});
 
 export const collections = {
-  posts
-}
+  posts,
+};
