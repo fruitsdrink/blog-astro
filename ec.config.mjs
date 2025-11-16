86c4809c984d9c03ad3a00b72717f4a9e97084c1
fruitsdrink <fruitsdrink@126.com>
2025-11-16 21:33:45 +0800

    Enhance ec.config.mjs with new plugins and update package dependencies
    - Added `pluginCollapsibleSections` and `pluginFullscreen` to the configuration for improved code presentation and user interaction.
    - Updated `package.json` to include new dependencies for the added plugins.
    - Modified `pnpm-lock.yaml` to reflect the new package versions and ensure proper dependency management.
    - Updated an example post to include a title for the code block, enhancing clarity in the documentation.

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,8 +1,20 @@
 import { defineEcConfig } from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginFullscreen } from "expressive-code-fullscreen";
 
 export default defineEcConfig({
-  plugins: [pluginLineNumbers()],
+  plugins: [
+    pluginCollapsibleSections(),
+    pluginFullscreen({
+      fullscreenButtonTooltip: "切换全屏",
+      enableEscapeKey: true,
+      exitOnBrowserBack: true,
+      animationDuration: 250,
+      addToUntitledBlocks: false, // Only show on blocks with titles.
+    }),
+    pluginLineNumbers(),
+  ],
   defaultProps: {
     showLineNumbers: false,
     overridesByLang: {
